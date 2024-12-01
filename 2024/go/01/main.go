a1ff21f3a70346dcb32f9ffea0d5c46bf7ac03b1
mbroton <michal.broton.ns@gmail.com>
2024-12-01 20:43:40 +0100

    Add day 1 part 2 solution

--- 2024/go/01/main.go
+++ 2024/go/01/main.go
@@ -48,12 +48,26 @@ func main() {
 
 	distance := 0
 	var d int
-	for i := range len(a) {
+	for i := range a {
 		d = a[i] - b[i]
 		distance += max(d, -d)
 	}
 
+	// part 1 solution
 	fmt.Println("Distance:", distance)
+
+	counter := make(map[int]int, 0)
+	for _, v := range b {
+		counter[v]++
+	}
+
+	similarityScore := 0
+	for _, v := range a {
+		similarityScore += v * counter[v]
+	}
+
+	// part 2 solution
+	fmt.Println("Similarity score:", similarityScore)
 }
 
 func check(e error) {
